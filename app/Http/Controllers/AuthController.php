a7dbaed21e09be3d1dae45279f2a504591bffc67
Abesse Smahi <contact@smahi.net>
2024-08-19 18:31:49 +0100

    Add AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        return response([
+            'message' => 'Hello from API'
+        ], 200);
+    }
+}
